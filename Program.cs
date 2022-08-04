7a1ef07a23dd32995fcfa6ea8da9625087046f5f
kappalpadikkalvivek <vivek@27technologies.com>
2022-08-04 15:47:54 +0530

    EV setup
    setting up the environment

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace BDProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BDProject
 Its a project to study CMS
+login details
+pass and user-bdproject@123.com
+db pass and user BDProject
